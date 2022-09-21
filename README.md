e435f7d1c2a666836ca9fa3b19255c6f74ea5ced
Wolf-Peter Bolduan <wolf-peter@bolduan.net>
2022-09-21 13:57:22 +0200

    etl.ipynb works!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 todo: add comment that decided to DOTHINING when the entry is updated on same id
+add the comment to the feedback that it would be better to build song and artist db completely before the last step in etl.ipynb
+the fact that I add the conversion of the timestamp to the dataframe allows to use the same format for the songplay table
--- sql_queries.py
+++ sql_queries.py
@@ -8,9 +8,10 @@ time_table_drop = "Drop table if exists time"
 
 # CREATE TABLES
 
+# here I checked a bit the FAQ and found "SERIAL"for the primary key
 songplay_table_create = "CREATE TABLE IF NOT EXISTS songplays \
-    (songplay_id varchar PRIMARY KEY, start_time varchar, user_id varchar, \
-    level varchar, song_id varchar, artist_id varchar, session_id varchar, user_agent varchar);" 
+    (songplay_id SERIAL PRIMARY KEY, start_time timestamp, user_id varchar, \
+    level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);" 
 
 user_table_create = "CREATE TABLE IF NOT EXISTS users \
     (user_id varchar PRIMARY KEY, first_name varchar, last_name varchar, \
@@ -28,21 +29,20 @@ time_table_create = "CREATE TABLE IF NOT EXISTS time \
     day int, week int, month int, year int, weekday int);"
 
 # INSERT RECORDS 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) """)
 
-user_table_insert = "INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING" 
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING""") 
 
 song_table_insert = "INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING" 
 
 artist_table_insert = "INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING" 
 
-time_table_insert = "INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+time_table_insert = "INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
 # FIND SONGS
+# Implement the `song_select` query in `sql_queries.py` to find the song ID and artist ID based on the title, artist name, and duration of a song.
 
-song_select = ("""
-""")
+song_select = song_select = ("""SELECT song_id, artists.artist_id FROM songs JOIN artists ON songs.artist_id = artists.artist_id WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s""")
 
 # QUERY LISTS
 

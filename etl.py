1385770db2e397335a6c617aed3e001e4ad3512d
Wolf-Peter Bolduan <wolf-peter@bolduan.net>
2022-09-22 14:11:32 +0200

    Second submission Udacity after review

--- etl.py
+++ etl.py
@@ -6,10 +6,17 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    # open song file
-    df = pd.read_json(filepath, lines=True)
+    '''
+    Uploads the extracted data from the song file to the song table and to the artist table of the database.
 
-    # insert song record
+            Parameters:
+                    cur: cursor to the opened database 
+                    filepath: filepath of the json song_file to be extracted
+
+            Returns:
+                    None, directly updates the postgresql database for song and artist data
+    '''
+    df = pd.read_json(filepath, lines=True)
     song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values
     song_data = song_data.tolist()[0]
     cur.execute(song_table_insert, song_data)
@@ -20,18 +27,20 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
-    # open log file
+    '''
+    Uploads the extracted data from the log file to the time table, user table and songplay table of the database.
+
+            Parameters:
+                    cur: cursor to the opened database 
+                    filepath: filepath of the json log_file to be extracted
+
+            Returns:
+                    None, directly updates the postgresql database for time, user and songplay information
+    '''
+
     df = pd.read_json(filepath, lines=True)
-
-    # filter by NextSong action
     df = df[df['page']=='NextSong']
-
-    # convert timestamp column to datetime
     df['ts_datetime']=pd.to_datetime(df['ts'], unit='ms')
-    
-    # insert time data records
-    # took a bit different approach here and added the time
-    # information to the original dataframe
     df['timestamp'] = df['ts_datetime']
     df['hour'] = df['ts_datetime'].dt.hour
     df['day'] = df['ts_datetime'].dt.day
@@ -43,18 +52,12 @@ def process_log_file(cur, filepath):
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
-
-    # load user table
     user_df = df[['userId', 'firstName','lastName', 'gender', 'level']]
 
-    # insert user records
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
-    # insert songplay records
     for index, row in df.iterrows():
-        
-        # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         
@@ -62,25 +65,30 @@ def process_log_file(cur, filepath):
             songid, artistid = results
         else:
             songid, artistid = None, None
-
-        # insert songplay record
         songplay_data = [row.timestamp, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
-    # get all files matching extension from directory
+    '''
+    Iterates over the files and calls each function for the data extraction for log files and song files
+
+            Parameters:
+                    conn: connection to the postgresql database
+                    cur: cursor to the opened database 
+                    filepath: filepath of the json log_files to be extracted
+                    func: function to be called for extraction of data (process_song_file, process_log_file)
+
+            Returns:
+                    None
+    '''
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
         for f in files :
             all_files.append(os.path.abspath(f))
-
-    # get total number of files found
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
-
-    # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
